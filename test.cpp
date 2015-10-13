bec509d04d5673a1ee731010ddfe2dc7deff0054
BlahGeek <i@BlahGeek.com>
2015-10-13 09:45:40 +0800

    init autoconf

---
+++ test.cpp
@@ -0,0 +1,16 @@
+/* 
+* @Author: BlahGeek
+* @Date:   2015-10-13
+* @Last Modified by:   BlahGeek
+* @Last Modified time: 2015-10-13
+*/
+
+#include <iostream>
+
+using namespace std;
+
+namespace vr {
+    void map() {
+        std::cout << "Hi there!" << std::endl;
+    }
+}
