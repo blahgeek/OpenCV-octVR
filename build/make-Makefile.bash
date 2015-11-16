b28d10953d9884a7c451b3d4ea48ad681f8a70ee
BlahGeek <i@blahgeek.com>
2015-11-16 09:51:26 +0800

    add make-Makefile.bash

---
+++ build/make-Makefile.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# GTX 960, see https://developer.nvidia.com/cuda-gpus
+cmake \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=`pwd`/../.. \
+    -D WITH_FFMPEG=OFF \
+    -D CMAKE_CXX_FLAGS="-DCV_OPENCL_RUN_VERBOSE" \
+    -D WITH_CUDA=ON \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCUDA_ARCH_BIN="5.2" \
+    -DCUDA_ARCH_PTX="5.2" \
+    ..
