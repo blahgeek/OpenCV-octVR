2f9b5ecf38f5876f676185f4c71833c53a1b9db6
BlahGeek <i@blahgeek.com>
2015-11-30 09:56:36 +0800

    use c++11, fuck c++

--- build/make-Makefile.bash
+++ build/make-Makefile.bash
@@ -4,10 +4,11 @@ cmake \
     -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_INSTALL_PREFIX=`pwd`/../.. \
     -D WITH_FFMPEG=OFF \
-    -D CMAKE_CXX_FLAGS="-DCV_OPENCL_RUN_VERBOSE" \
+    -D CMAKE_CXX_FLAGS="-std=c++11 -DCV_OPENCL_RUN_VERBOSE" \
+    -D CUDA_NVCC_FLAGS="-std=c++11 --expt-relaxed-constexpr" \
     -D WITH_CUDA=ON \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DCUDA_ARCH_BIN="5.2" \
-    -DCUDA_ARCH_PTX="5.2" \
-    -DWITH_OPENCL=OFF \
+    -D BUILD_SHARED_LIBS=OFF \
+    -D CUDA_ARCH_BIN="5.2" \
+    -D CUDA_ARCH_PTX="5.2" \
+    -D WITH_OPENCL=OFF \
     ..
