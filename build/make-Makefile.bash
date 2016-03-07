1a73cbfb727bf915e63d86701af2b4acce43db21
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-03-07 09:18:12 +0800

    Modify Linux build script: move unnecessary IPP, OPENEXR, VFW and python

--- build/make-Makefile.bash
+++ build/make-Makefile.bash
@@ -3,6 +3,7 @@
 cmake \
     -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_INSTALL_PREFIX=`pwd`/../.. \
+    -D WITH_IPP=OFF \
     -D WITH_FFMPEG=OFF \
     -D CMAKE_CXX_FLAGS="-std=c++11 -DCV_OPENCL_RUN_VERBOSE" \
     -D CUDA_NVCC_FLAGS="-std=c++11 --expt-relaxed-constexpr" \
@@ -10,8 +11,12 @@ cmake \
     -D BUILD_SHARED_LIBS=ON \
     -D CUDA_ARCH_BIN="5.2" \
     -D CUDA_ARCH_PTX="5.2" \
-    -D WITH_OPENCL=OFF \
+    -D WITH_OPENCL=ON \
+    -D WITH_OPENEXR=OFF \
+    -D WITH_VFW=OFF \
     -D BUILD_TESTS=OFF \
     -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_opencv_python2=OFF \
+    -D BUILD_opencv_python3=OFF \
     $@ \
     ..
--- build/make-Makefile.no-cuda.bash
+++ build/make-Makefile.no-cuda.bash
@@ -3,13 +3,17 @@
 cmake \
     -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_INSTALL_PREFIX=`pwd`/../.. \
+    -D WITH_IPP=OFF \
     -D WITH_FFMPEG=OFF \
     -D CMAKE_CXX_FLAGS="-std=c++11 -DCV_OPENCL_RUN_VERBOSE" \
     -D WITH_CUDA=OFF \
     -D BUILD_SHARED_LIBS=ON \
     -D WITH_OPENCL=ON \
     -D WITH_OPENEXR=OFF \
+    -D WITH_VFW=OFF \
     -D BUILD_TESTS=OFF \
     -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_opencv_python2=OFF \
+    -D BUILD_opencv_python3=OFF \
     $@ \
     ..
