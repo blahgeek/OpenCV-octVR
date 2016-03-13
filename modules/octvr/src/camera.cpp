7901aaeaacd3c1fe3b72730ac5bb0a564bdec897
Feng Zhoutian <fengzhoutian@hotmail.com>
2016-03-14 00:56:21 +0800

    Revert "Merge branch 'octVR-ocam' into octVR"
    This reverts commit df8a5efc80ece0a18c573df0074250e7d4531604, reversing
    changes made to 4d475a84913c9fa087985b886a2b91d2369ca850.
    
    Conflicts:
    	modules/octvr/src/camera.cpp

--- modules/octvr/src/camera.cpp
+++ modules/octvr/src/camera.cpp
@@ -19,7 +19,6 @@
 #include "./cameras/cubic.hpp"
 #include "./cameras/eqareanorthpole.hpp"
 #include "./cameras/eqareasouthpole.hpp"
-#include "./cameras/ocam_fisheye.hpp"
 
 using namespace vr;
 
@@ -34,7 +33,6 @@ std::unique_ptr<Camera> Camera::New(const std::string & type, const rapidjson::V
     X("fisheye", FisheyeCamera)
     X("equirectangular", Equirectangular)
     X("fullframe_fisheye", FullFrameFisheyeCamera)
-    X("ocam_fisheye", OCamFisheyeCamera)
     X("stupidoval", StupidOval)
     X("cubic", Cubic)
     X("eqareanorthpole", Eqareanorthpole)
