07e2a378339ad216c95a05ad3e3949750637c94f
BlahGeek <i@BlahGeek.com>
2016-04-05 13:18:03 +0800

    fix video preview

--- apps/livestitching/preview_video.hpp
+++ apps/livestitching/preview_video.hpp
@@ -2,7 +2,7 @@
 * @Author: BlahGeek
 * @Date:   2016-02-23
 * @Last Modified by:   BlahGeek
-* @Last Modified time: 2016-02-26
+* @Last Modified time: 2016-04-05
 */
 
 #ifndef LIVESTITCHING_PREVIEW_VIDEO_H__
@@ -31,9 +31,9 @@ protected:
     void paintEvent(QPaintEvent *event);
 
 public:
-    PreviewVideoWidget(QWidget * parent=nullptr,
-                       int preview_w=1280, int preview_h=640);
+    PreviewVideoWidget(QWidget * parent=nullptr);
     bool isValid() { return valid_shared_memory; }
+    void prepare(int _w, int _h);
 
 public slots:
     void updatePreview();
