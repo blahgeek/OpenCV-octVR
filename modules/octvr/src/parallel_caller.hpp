f76d54f4dfb72f03075b00ea0dab62cbc0a04e72
BlahGeek <i@BlahGeek.com>
2016-04-08 11:04:00 +0800

    use block scheduler, faster

--- modules/octvr/src/parallel_caller.hpp
+++ modules/octvr/src/parallel_caller.hpp
@@ -1,13 +1,15 @@
 /* 
 * @Author: StrayWarrior
 * @Date:   2016-04-01
-* @Last Modified by:   StrayWarrior 
-* @Last Modified time: 2016-04-02
+* @Last Modified by:   BlahGeek
+* @Last Modified time: 2016-04-08
 */
 
 #ifndef VR_PARALLEL_CALLER_H
 #define VR_PARALLEL_CALLER_H value
 
+#include <cmath>
+#include <algorithm>
 #include <opencv2/core/utility.hpp>
 
 namespace vr {
@@ -16,17 +18,24 @@ template <typename Body>
 class ParallelFunctionCaller : public cv::ParallelLoopBody {
 private:
     const Body & _func;
+    int block_size;
+    cv::Range elem_range;
 public:
-    ParallelFunctionCaller(const Body& _parallel_func) : _func(_parallel_func) {}
+    ParallelFunctionCaller(const Body& _parallel_func, int _bs, cv::Range _r) : 
+        _func(_parallel_func), block_size(_bs), elem_range(_r) {}
     void operator() (const cv::Range& _range) const
     {
-        _func(_range);
+        cv::Range real_range(std::max(_range.start * block_size, elem_range.start), 
+                             std::min(_range.end * block_size, elem_range.end));
+        _func(real_range);
     }
 };
 
 template <typename Body>
-void parallel_for_caller(const cv::Range& _range, const Body& _body) {
-    cv::parallel_for_(_range, ParallelFunctionCaller<Body>(_body));
+void parallel_for_caller(const cv::Range& _range, const Body& _body, int block_size=64) {
+    cv::Range _block_range(std::floor(_range.start / float(block_size)),
+                           std::ceil(_range.end / float(block_size)));
+    cv::parallel_for_(_block_range, ParallelFunctionCaller<Body>(_body, block_size, _range));
 }
 
 }
