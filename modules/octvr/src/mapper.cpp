68496565c0f6d5dd3a9bd7237d946fab91ee78d0
BlahGeek <i@BlahGeek.com>
2016-04-14 10:45:33 +0800

    fix logo color, new equirectangular logo

--- modules/octvr/src/mapper.cpp
+++ modules/octvr/src/mapper.cpp
@@ -2,10 +2,12 @@
 * @Author: BlahGeek
 * @Date:   2015-10-13
 * @Last Modified by:   BlahGeek
-* @Last Modified time: 2016-03-10
+* @Last Modified time: 2016-04-14
 */
 
 #include <iostream>
+#include <algorithm>
+#include <utility>
 #include "./mapper.hpp"
 
 #include "opencv2/core.hpp"
@@ -69,6 +71,8 @@ Mapper::Mapper(const MapperTemplate & mt, std::vector<cv::Size> in_sizes,
     cv::split(logo_png, logo_channels);
     logo_mask_mat = logo_channels[3];
     logo_channels.pop_back();
+
+    std::swap(logo_channels[0], logo_channels[2]);
     cv::merge(logo_channels, logo_data_mat);
 
     GpuMat logo_data_tmp, logo_mask_tmp;
